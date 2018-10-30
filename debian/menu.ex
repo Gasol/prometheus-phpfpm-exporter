35a94276b254ddf262ac389c82d17705d320c660
Gasol Wu <gasol.wu@gmail.com>
2018-10-30 22:05:24 +0800

    Remove templates

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(prometheus-phpfpm-exporter):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="prometheus-phpfpm-exporter" command="/usr/bin/prometheus-phpfpm-exporter"
--- debian/prometheus-phpfpm-exporter.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the prometheus-phpfpm-exporter package
-#
-0 4	* * *	root	[ -x /usr/bin/prometheus-phpfpm-exporter_maintenance ] && /usr/bin/prometheus-phpfpm-exporter_maintenance
